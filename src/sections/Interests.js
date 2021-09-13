f67a948e75786c6afadb50c8daad2aa7b3e32349
ezza022 <alfirman.pahlepi@gmail.com>
2021-09-14 01:27:46 +0700

    add interests section

--- src/sections/Interests.js
+++ src/sections/Interests.js
@@ -1,5 +1,16 @@
 const Interests = () => {
-  return <div id="interests" className="h-screen">interest</div>;
+  return (
+    <div
+      id="interests"
+      className="h-screen flex flex-col justify-center space-y-10"
+    >
+      <h1 className="uppercase text-6xl font-bold text-gray-800">interests</h1>
+      <p className="text-gray-400 whitespace-pre-line">{interest}</p>
+    </div>
+  );
 };
+
+const interest =
+  "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever sinceLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since \n\nand typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever sinceLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since";
 
 export default Interests;

5535f33f925e446f540ef1ca943a5ea89e0f3e86
ezza022 <alfirman.pahlepi@gmail.com>
2021-09-13 23:55:47 +0700

    add Experience.js section add avatar img url add link button

--- src/components/Avatar.js
+++ src/components/Avatar.js
@@ -1,8 +1,14 @@
 const Avatar = () => (
   <div
-    className="h-48 w-48 rounded-full bg-white"
+    className="h-48 w-48 rounded-full bg-white overflow-hidden"
     style={{ border: "7px solid rgba(253, 230, 138,0.99)" }}
-  ></div>
+  >
+    <img
+      src="https://avatars.githubusercontent.com/u/78687274?v=4"
+      alt="avatar"
+      className="h-full w-full object-cover"
+    />
+  </div>
 );
 
 export default Avatar;
--- src/sections/Experience.js
+++ src/sections/Experience.js
@@ -1,9 +1,53 @@
 const Experience = () => {
   return (
-    <div id="experience" className="h-screen">
-      experience
+    <div id="experience" className="h-screen py-10">
+      <h1 className="uppercase text-5xl font-bold text-gray-800 mb-10">experience</h1>
+      <ul className="space-y-6">
+        {datas.map((data, index) => (
+          <li key={index} className="flex">
+            <div className="w-2/3">
+              <h2 className="uppercase text-2xl font-bold text-gray-800">
+                {data.status}
+              </h2>
+              <h3 className="text-xl uppercase font-bold text-gray-500 mt-1">
+                {data.agency}
+              </h3>
+              <p className="font-semibold text-gray-500 mt-3">
+                {data.description}
+              </p>
+            </div>
+            <p className="text-red-400 text-lg text-right w-1/3 font-semibold">
+              {data.time}
+            </p>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
+
+const datas = [
+  {
+    status: "Github campus expert",
+    agency: "github",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since",
+    time: "August 2018 - Present",
+  },
+  {
+    status: "Github campus expert",
+    agency: "github",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since",
+    time: "August 2018 - Present",
+  },
+  {
+    status: "Github campus expert",
+    agency: "github",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since",
+    time: "August 2018 - Present",
+  },
+];
 
 export default Experience;

9280aeabeb32dc8db0063145d777a2e79a4b3663
ezza022 <alfirman.pahlepi@gmail.com>
2021-09-14 01:59:51 +0700

    handle responsive

--- src/App.js
+++ src/App.js
@@ -9,20 +9,25 @@ import Awards from "./sections/Awards";
 
 function App() {
   return (
-    <div className="min-h-screen min-w-screen bg-white flex justify-end">
-      <aside className="h-screen w-1/4 bg-yellow-300 fixed flex flex-col items-center justify-center space-y-6 left-0 top-0">
-        <Avatar />
-        <NavLinks />
-      </aside>
-      <main className="w-3/4 px-10">
-        <About />
-        <Experience />
-        <Education />
-        <Skills />
-        <Interests />
-        <Awards />
-      </main>
-    </div>
+    <>
+      <div className="min-h-screen min-w-screen bg-white justify-end hidden lg:flex">
+        <aside className="h-screen w-1/4 bg-yellow-300 fixed flex flex-col items-center justify-center space-y-6 left-0 top-0">
+          <Avatar />
+          <NavLinks />
+        </aside>
+        <main className="w-3/4 px-10">
+          <About />
+          <Experience />
+          <Education />
+          <Skills />
+          <Interests />
+          <Awards />
+        </main>
+      </div>
+      <p className="text-red-400 font-semibold text-2xl block lg:hidden">
+        sorry this page not responsive :(
+      </p>
+    </>
   );
 }
 

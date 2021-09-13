7f9042caa529e4e88db3baffb7ed74b95dd1e359
ezza022 <alfirman.pahlepi@gmail.com>
2021-09-13 14:40:54 +0700

    left side

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p className="text-green-200">
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main className="min-h-screen min-w-screen bg-gray-100 flex justify-end">
+      <div className="h-screen w-1/4 bg-yellow-300 fixed flex flex-col items-center justify-center space-y-6 left-0 top-0">
+        <div
+          className="h-48 w-48 rounded-full bg-white"
+          style={{ border: "7px solid rgba(253, 230, 138,0.99)" }}
+        ></div>
+        <div className="text-center space-y-2">
+          {links.map((link, index) => (
+            <p className="uppercase font-semibold text-xl text-gray-200 hover:text-white cursor-pointer" key={index}>
+              {link}
+            </p>
+          ))}
+          <p></p>
+        </div>
+      </div>
+      <div className="w-3/4">asddsa</div>
+    </main>
   );
 }
+
+const links = [
+  "about",
+  "experience",
+  "education",
+  "skills",
+  "interest",
+  "awards",
+];
 
 export default App;

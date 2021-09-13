5cdc2d6ac38d9f24485274d3a9738806496d43e9
ezza022 <alfirman.pahlepi@gmail.com>
2021-09-14 01:47:14 +0700

    add awards section

--- src/sections/Awards.js
+++ src/sections/Awards.js
@@ -1,3 +1,5 @@
+import MedalIcon from "../components/MedalIcon";
+
 const Awards = () => (
   <section
     id="awards"
@@ -6,7 +8,21 @@ const Awards = () => (
     <h1 className="uppercase text-5xl font-bold text-gray-800">
       awards & certifications
     </h1>
+    <ul className="space-y-1 px-10">
+      {awards.map((award, index) => (
+        <li key={index} className="flex items-center space-x-2">
+          <MedalIcon />
+          <span className=" font-semibold text-gray-400">{award}</span>
+        </li>
+      ))}
+    </ul>
   </section>
 );
+
+const awards = [
+  "Hack 36 3rd prize winner",
+  "Mentor OpenCode'19",
+  "Started North Indonesia First Student Design and Developer Conference",
+];
 
 export default Awards;

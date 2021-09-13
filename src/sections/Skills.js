6fb9c9632aaa402771ca67066a80f710e0aced01
ezza022 <alfirman.pahlepi@gmail.com>
2021-09-14 01:13:20 +0700

    add skills section

--- src/sections/Skills.js
+++ src/sections/Skills.js
@@ -1,5 +1,38 @@
+import CheckIcon from "../components/CheckIcon";
+
 const Skills = () => {
-  return <div id="skills" className="h-screen">skills</div>;
+  return (
+    <div id="skills" className="h-screen flex flex-col justify-center">
+      <h1 className="uppercase text-4xl font-bold text-gray-800 mb-10">
+        skills
+      </h1>
+      <h3 className="uppercase text-2xl font-bold text-gray-400 mb-5">
+        programming languages & tools
+      </h3>
+      <ul className="grid grid-cols-2 gap-2">
+        {datas.map((data, index) => (
+          <li
+            key={index}
+            className="text-xl font-semibold text-gray-400 flex items-center space-x-2"
+          >
+            <CheckIcon />
+            <span>{data}</span>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 };
+
+const datas = [
+  "HTML",
+  "CSS",
+  "Javascript",
+  "MongoDB",
+  "Express js",
+  "React js",
+  "Node js",
+  "Next js",
+];
 
 export default Skills;
